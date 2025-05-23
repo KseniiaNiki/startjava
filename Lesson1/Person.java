a304dac53aae16a5aa8f8b8797d964bb23d73f40
Ksenia Nikienko <ksysic@mail.ru>
2025-05-24 01:34:50 +0300

    Выполнено ДЗ на тему "Класс"

---
+++ Lesson1/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    
+    String sex = "Female";
+    String name = "Anna";
+    int height = 167;
+    int weight = 56;
+    int age = 27;
+
+    void walk() {
+        System.out.println("Хожу");
+    }
+
+    void sit() {
+        System.out.println("Сижу");
+    }
+
+    void run() {
+        System.out.println("Бегу");
+    }
+
+    void speak() {
+        System.out.println("Говорю");
+    }
+
+    void learnJava() {
+        System.out.println("Учу Java");
+    }
+}
