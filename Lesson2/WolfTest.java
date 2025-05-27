9638ac10d86222907586d8ee20a7adf34787bfea
Ksenia Nikienko <ksysic@mail.ru>
2025-05-27 03:33:57 +0300

    Выполнено ДЗ на тему "Инкапсуляция: геттеры/сеттеры"
    изменены файлы Wolf.java, WolfTest.java

--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -2,14 +2,15 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        wolfOne.sex = "мужской";
-        wolfOne.nickame = "Альфа";
-        wolfOne.weight = 70;
-        wolfOne.age = 3;
-        wolfOne.color = "серый";
-        System.out.println("Первого волка зовут " + wolfOne.nickame);
-        System.out.println("Его характеристики: " + wolfOne.sex + " пол, " + 
-                wolfOne.weight + " кг, " + wolfOne.age + " года, " + wolfOne.color + " цвет");
+        wolfOne.setSex("мужской");
+        wolfOne.setNickname("Альфа");
+        wolfOne.setWeight(70);
+        wolfOne.setAge(3);
+        wolfOne.setColor("серый");
+        System.out.println("Первого волка зовут " + wolfOne.getNickname());
+        System.out.println("Его характеристики: " + wolfOne.getSex() + " пол, " + 
+                wolfOne.getWeight() + " кг, " + wolfOne.getAge() + " года, " + 
+                wolfOne.getColor() + " цвет");
         wolfOne.walk();
         wolfOne.sit();
         wolfOne.run();
