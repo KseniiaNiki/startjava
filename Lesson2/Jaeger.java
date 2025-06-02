d125297744e192ba888fd4a4634fdad7ac872503
Ksenia Nikienko <ksysic@mail.ru>
2025-06-02 03:43:01 +0300

    Выполнено ДЗ с названием МОДИФИЦИРУЙТЕ “JAEGER“

---
+++ Lesson2/Jaeger.java
@@ -0,0 +1,87 @@
+public class Jaeger {
+    private String modelName;
+    private String mark;
+    private String origin;
+    private double height;
+    private double weight;
+    private int speed;
+    private int strength;
+    private int armor;
+    private String bodyLanguage;
+
+    public Jaeger() {
+    }
+
+    public Jaeger(String modelName, String mark, String origin, double height, double weight, 
+            int speed, int strength, int armor, String bodyLanguage) {
+        this.modelName = modelName;
+        this.mark = mark;
+        this.origin = origin;
+        this.height = height;
+        this.weight = weight;
+        this.speed = speed;
+        this.strength = strength;
+        this.armor = armor;
+        this.bodyLanguage = bodyLanguage;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public void setBodyLanguage(String bodyLanguage) {
+        this.bodyLanguage = bodyLanguage;
+    }
+
+    public void presentation() {
+        System.out.println("Робот " + modelName + " имеет следующие характеристики: ");
+        System.out.println("марка - " + mark + ", происхождение - " + origin + 
+                ", высота - " + height + " метра(ов), вес - " + weight + 
+                " тонн, скорость - " + speed + ", сила - " + strength + 
+                ", бронь - " + armor + ", язык тела - " + bodyLanguage);
+    }
+
+    public void drift() {
+        System.out.println("Нейронная синхронизация");
+    }
+
+    public void move() {
+        System.out.println("Перемещается");
+    }
+
+    public String scanKaiju() {
+        return "Сканирование наличия Кайдзю";
+    }
+
+    public void usePlasmacaster() {
+        System.out.println("Plasmacaster активирован");
+    }
+}
---
+++ Lesson2/JaegerTest.java
@@ -0,0 +1,27 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger jaegerOne = new Jaeger();
+        jaegerOne.setModelName("Crimson Typhoon");
+        jaegerOne.setMark("Mark-4");
+        jaegerOne.setOrigin("Китай");
+        jaegerOne.setHeight(76.2);
+        jaegerOne.setWeight(1722);
+        jaegerOne.setSpeed(9);
+        jaegerOne.setStrength(8);
+        jaegerOne.setArmor(6);
+        jaegerOne.setBodyLanguage("Muay Thai triplet");
+        jaegerOne.presentation();
+        jaegerOne.drift();
+        jaegerOne.move();
+        System.out.println(jaegerOne.scanKaiju());
+        jaegerOne.usePlasmacaster();
+
+        Jaeger jaegerTwo = new Jaeger("Gipsy Danger", "Mark-3", "США", 
+                79.25, 1980, 7, 8, 6, "Street Fighter");
+        jaegerTwo.presentation();
+        jaegerTwo.drift();
+        jaegerTwo.move();
+        System.out.println(jaegerTwo.scanKaiju());
+        jaegerTwo.usePlasmacaster();
+    }
+}
