feda9c0ae4d5d2f384e45c5fdb13d6d91d165582
Ksenia Nikienko <ksysic@mail.ru>
2025-06-08 01:00:39 +0300

    Финишная доработка

--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class GuessNumberTest {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in, "cp866");
         System.out.print("Введите имя первого игрока: ");
         Player playerOne = new Player(scanner.nextLine());
         System.out.print("Введите имя второго игрока: ");
@@ -12,12 +12,10 @@ public class GuessNumberTest {
         String answer = "yes";
         while (answer.equals("yes")) {
             game.start();
-            System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-            answer = scanner.nextLine();
-            while (!answer.equals("no") && !answer.equals("yes")) {
+            do {
                 System.out.print("Хотите продолжить вычисления? [yes/no]: ");
                 answer = scanner.nextLine();
-            }
+            } while (!answer.equals("no") && !answer.equals("yes"));
         }
     }
 }
