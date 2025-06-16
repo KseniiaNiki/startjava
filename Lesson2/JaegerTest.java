f897bd40445e0361fb783ba12d1f01c122a56d4d
Ksenia Nikienko <ksysic@mail.ru>
2025-06-17 02:01:17 +0300

    Приведение к эталону

--- Lesson2/JaegerTest.java
+++ Lesson2/JaegerTest.java
@@ -1,27 +1,27 @@
 public class JaegerTest {
     public static void main(String[] args) {
-        Jaeger jaegerOne = new Jaeger();
-        jaegerOne.setModelName("Crimson Typhoon");
-        jaegerOne.setMark("Mark-4");
-        jaegerOne.setOrigin("Китай");
-        jaegerOne.setHeight(76.2);
-        jaegerOne.setWeight(1722);
-        jaegerOne.setSpeed(9);
-        jaegerOne.setStrength(8);
-        jaegerOne.setArmor(6);
-        jaegerOne.setBodyLanguage("Muay Thai triplet");
-        System.out.println(jaegerOne.toString());
-        jaegerOne.drift();
-        jaegerOne.move();
-        System.out.println(jaegerOne.scanKaiju());
-        jaegerOne.usePlasmacaster();
+        Jaeger crimsonTyphoon = new Jaeger();
+        crimsonTyphoon.setModelName("Crimson Typhoon");
+        crimsonTyphoon.setMark("Mark-4");
+        crimsonTyphoon.setOrigin("Китай");
+        crimsonTyphoon.setHeight(76.2);
+        crimsonTyphoon.setWeight(1722);
+        crimsonTyphoon.setSpeed(9);
+        crimsonTyphoon.setStrength(8);
+        crimsonTyphoon.setArmor(6);
+        crimsonTyphoon.setBodyLanguage("Muay Thai triplet");
+        System.out.println(crimsonTyphoon);
+        crimsonTyphoon.drift();
+        crimsonTyphoon.move();
+        System.out.println(crimsonTyphoon.scanKaiju());
+        crimsonTyphoon.usePlasmacaster();
 
-        Jaeger jaegerTwo = new Jaeger("Gipsy Danger", "Mark-3", "США", 
+        Jaeger gipsyDanger = new Jaeger("Gipsy Danger", "Mark-3", "США", 
                 79.25, 1980, 7, 8, 6, "Street Fighter");
-        System.out.println(jaegerTwo.toString());
-        jaegerTwo.drift();
-        jaegerTwo.move();
-        System.out.println(jaegerTwo.scanKaiju());
-        jaegerTwo.usePlasmacaster();
+        System.out.println(gipsyDanger);
+        gipsyDanger.drift();
+        gipsyDanger.move();
+        System.out.println(gipsyDanger.scanKaiju());
+        gipsyDanger.usePlasmacaster();
     }
 }
