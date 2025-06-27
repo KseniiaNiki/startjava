7077b089784bd59caa027d4877ee6947499a9fc5
Ksenia Nikienko <ksysic@mail.ru>
2025-06-28 01:14:12 +0300

    Возвращение предыдущего названия класса, содержащего статический метод

--- src/ru/topjava/startjava/lesson_2_3/method/naming/BooleanMethods.java
+++ src/ru/topjava/startjava/lesson_2_3/method/naming/BooleanMethods.java
@@ -1,56 +1,56 @@
-package ru.topjava.startjava.lesson_2_.naming;
+package ru.topjava.startjava.lesson_2_3.method.naming;
 
 public class BooleanMethods {
     public boolean shouldStopProgram() {
-        System.out.print(CurrentMethodNameUtil.getCurrentMethodName() + 
+        System.out.print(MethodUtils.getCurrentMethodName() + 
                 "() -> программа выполняется далее или завершается? ");
         return false;
     }
 
     public boolean hasUniqueDigit() {
-        System.out.print(CurrentMethodNameUtil.getCurrentMethodName() + 
+        System.out.print(MethodUtils.getCurrentMethodName() + 
                 "() -> последовательность содержит уникальную цифру? ");
         return true;
     }
 
     public boolean isLetter() {
-        System.out.print(CurrentMethodNameUtil.getCurrentMethodName() + 
+        System.out.print(MethodUtils.getCurrentMethodName() + 
                 "() -> пользователь ввел букву или что-то другое? ");
         return true;
     }
 
     public boolean hasEqualDigits() {
-        System.out.print(CurrentMethodNameUtil.getCurrentMethodName() + 
+        System.out.print(MethodUtils.getCurrentMethodName() + 
                 "() -> в проверяемых числах, есть равные цифры? ");
         return true;
     }
 
     public boolean hasAttempts() {
-        System.out.print(CurrentMethodNameUtil.getCurrentMethodName() + 
+        System.out.print(MethodUtils.getCurrentMethodName() + 
                 "() -> в игре 'Марио' остались попытки? ");
         return true;
     }
 
     public boolean isEmpty() {
-        System.out.print(CurrentMethodNameUtil.getCurrentMethodName() + 
+        System.out.print(MethodUtils.getCurrentMethodName() + 
                 "() -> пользователь ввёл пустую строку или из одних пробелов? ");
         return true;
     }
 
     public boolean isEvenNumber() {
-        System.out.print(CurrentMethodNameUtil.getCurrentMethodName() + 
+        System.out.print(MethodUtils.getCurrentMethodName() + 
                 "() -> на кубике, который бросил компьютер, выпало четное число? ");
         return true;
     }
 
     public boolean isValidPath() {
-        System.out.print(CurrentMethodNameUtil.getCurrentMethodName() + 
+        System.out.print(MethodUtils.getCurrentMethodName() + 
                 "() -> путь до файла, который вы ищите на ssd, действительный? ");
         return true;
     }
 
     public boolean isExistFile() {
-        System.out.print(CurrentMethodNameUtil.getCurrentMethodName() + 
+        System.out.print(MethodUtils.getCurrentMethodName() + 
                 "() -> файл по указанному адресу существует? ");
         return true;
     }
--- src/ru/topjava/startjava/lesson_2_3/method/naming/MethodUtils.java
+++ src/ru/topjava/startjava/lesson_2_3/method/naming/MethodUtils.java
@@ -1,6 +1,6 @@
 package ru.topjava.startjava.lesson_2_3.method.naming;
 
-public class CurrentMethodNameUtil {
+public class MethodUtils {
     public static String getCurrentMethodName() {
         return Thread.currentThread().getStackTrace()[2].getMethodName();
     }
