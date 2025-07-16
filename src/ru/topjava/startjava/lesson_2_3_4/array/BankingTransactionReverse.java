f910b0088c9ba6b01d8a9e6b26935951142768d1
Ksenia Nikienko <ksysic@mail.ru>
2025-07-17 00:31:43 +0300

    Выполнено 1е ДЗ на тему "Массивы"

--- src/ru/topjava/startjava/lesson_2_3_4/array/BankingTransactionReverse.java
+++ src/ru/topjava/startjava/lesson_2_3_4/array/BankingTransactionReverse.java
@@ -3,33 +3,55 @@ package ru.topjava.startjava.lesson_2_3_4.array;
 import java.util.Arrays;
 
 public class BankingTransactionReverse {
-    public static void doReverse(int[] transactionSums) {
-        if (transactionSums.length == 0) {
+    public static int[] reverse(int[] transactions) {
+        if (transactions == null) {
+            return null;
+        }
+        int[] reversed = Arrays.copyOf(transactions, transactions.length);
+        int length = reversed.length;
+        for(int i = 0; i < length / 2; i++) {
+            int halfArray = reversed[i];
+            reversed[i] = reversed[length - i - 1];
+            reversed[length - i - 1] = halfArray;
+        }
+        return reversed;
+    }
+
+    public static void showResult(int[] transactions, int[] reversed) {
+        if (transactions == null) {
+            System.out.println("Ошибка в получении данных(никакой информации не получено)");
+            return;
+        }
+        if (transactions.length == 0) {
             System.out.println("Нет данных по транзакциям(ни одна операция не была выполнена)");
             return;
         }
-
-//        if (transactionSums = null) {
-//            System.out.println("Ошибка в получении данных(никакой информации не получено)");
-//            return;
-//        }
-
-        System.out.println("Исходные транзакции: " + Arrays.toString(transactionSums));
-
-        int[] reversedArray = Arrays.copyOf(transactionSums, transactionSums.length);
-        for(int i = 0; i < reversedArray.length / 2; i++) {
-            int halfArray = reversedArray[i];
-            reversedArray[i] = reversedArray[reversedArray.length - i - 1];
-            reversedArray[reversedArray.length - i - 1] = halfArray;
-        }
-        System.out.println(" В обратном порядке: " + Arrays.toString(reversedArray));
+        System.out.println("Исходные транзакции: " + Arrays.toString(transactions));
+        System.out.println(" В обратном порядке: " + Arrays.toString(reversed));
     }
 
     public static void main(String[] args) {
-        doReverse(new int[] {});
-//        doReverse(new int[]);
-        doReverse(new int[] {5});
-        doReverse(new int[] {6, 8, 9, 1});
-        doReverse(new int[] {13, 8, 5, 3, 2, 1, 1});
+        int[] transactions;
+        int[] reversed;
+
+        transactions = new int[] {};
+        reversed = reverse(transactions);
+        showResult(transactions, reversed);
+
+        transactions = null;
+        reversed = reverse(transactions);
+        showResult(transactions, reversed);
+
+        transactions = new int[] {5};
+        reversed = reverse(transactions);
+        showResult(transactions, reversed);
+
+        transactions = new int[] {6, 8, 9, 1};
+        reversed = reverse(transactions);
+        showResult(transactions, reversed);
+
+        transactions = new int[] {13, 8, 5, 3, 2, 1, 1};
+        reversed = reverse(transactions);
+        showResult(transactions, reversed);
     }
 }
