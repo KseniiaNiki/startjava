6838a3228a8ef50a1bd84f0e63434328eaf84160
Ksenia Nikienko <ksysic@mail.ru>
2025-07-20 01:23:32 +0300

    Выполнено 2е ДЗ на тему "Массивы"

--- src/ru/topjava/startjava/lesson_2_3_4/array/LoadingAnimation.java
+++ src/ru/topjava/startjava/lesson_2_3_4/array/LoadingAnimation.java
@@ -13,7 +13,7 @@ public class LoadingAnimation {
         }
     }
 
-    public class ConsoleColors {
+    public static class ConsoleColors {
         public static final String RESET = "\033[0m";
         public static final String RED = "\033[0;31m";
         public static final String GREEN = "\033[0;32m";
