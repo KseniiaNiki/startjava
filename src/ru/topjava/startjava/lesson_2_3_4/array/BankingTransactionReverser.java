1a32fa62efc16ae4db1f4b6e5ae6917d87f2cf72
Ksenia Nikienko <ksysic@mail.ru>
2025-07-20 01:03:23 +0300

    Доработка ДЗ и редактирование .gitignore

---
+++ src/ru/topjava/startjava/lesson_2_3_4/array/BankingTransactionReverser.java
@@ -0,0 +1,55 @@
+package ru.topjava.startjava.lesson_2_3_4.array;
+
+import java.util.Arrays;
+
+public class BankingTransactionReverser {
+    public static void main(String[] args) {
+        int[] transactions = new int[] {};
+        int[] reversed = reverse(transactions);
+        showResult(transactions, reversed);
+
+        transactions = null;
+        reversed = reverse(transactions);
+        showResult(transactions, reversed);
+
+        transactions = new int[] {5};
+        reversed = reverse(transactions);
+        showResult(transactions, reversed);
+
+        transactions = new int[] {6, 8, 9, 1};
+        reversed = reverse(transactions);
+        showResult(transactions, reversed);
+
+        transactions = new int[] {13, 8, 5, 3, 2, 1, 1};
+        reversed = reverse(transactions);
+        showResult(transactions, reversed);
+    }
+
+        private static int[] reverse(int[] transactions) {
+            if (transactions == null) {
+                return null;
+            }
+
+            int[] reversed = Arrays.copyOf(transactions, transactions.length);
+            int length = reversed.length;
+            for (int r : reversed) {
+                transactions[length - 1] = r;
+                length--;
+            }
+            return reversed;
+        }
+
+        private static void showResult(int[] transactions, int[] reversed) {
+            if (transactions == null) {
+                System.out.println("Ошибка в получении данных(никакой информации не получено)");
+                return;
+            }
+            if (transactions.length == 0) {
+                System.out.println("Нет данных по транзакциям(ни одна операция не была выполнена)");
+                return;
+            }
+            System.out.println("Исходные транзакции: " + Arrays.toString(transactions));
+            System.out.println(" В обратном порядке: " + Arrays.toString(reversed));
+        }
+
+}
