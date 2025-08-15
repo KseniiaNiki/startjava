08c138817571f65bcb69095332c07f149f672bbd
Ksenia Nikienko <ksysic@mail.ru>
2025-08-16 01:03:20 +0300

    Частичное редактирование

--- src/ru/topjava/startjava/lesson_2_3_4/array/FactorialCalculator.java
+++ src/ru/topjava/startjava/lesson_2_3_4/array/FactorialCalculator.java
@@ -1,29 +1,29 @@
 package ru.topjava.startjava.lesson_2_3_4.array;
 
-public class FactorialCalculation {
+public class FactorialCalculator {
     public static void main(String[] args) {
         int[] numbers = new int[] {};
-        long[] factorials = getFactorial(numbers);
+        long[] factorials = calculate(numbers);
         showCalculation(factorials, numbers);
 
         numbers = null;
-        factorials = getFactorial(numbers);
+        factorials = calculate(numbers);
         showCalculation(factorials, numbers);
 
         numbers = new int[] {8, 0, 9};
-        factorials = getFactorial(numbers);
+        factorials = calculate(numbers);
         showCalculation(factorials, numbers);
 
         numbers = new int[] {-3, 1, 7, 13};
-        factorials = getFactorial(numbers);
+        factorials = calculate(numbers);
         showCalculation(factorials, numbers);
 
         numbers = new int[] {-22, -0};
-        factorials = getFactorial(numbers);
+        factorials = calculate(numbers);
         showCalculation(factorials, numbers);
     }
 
-    public static long[] getFactorial(int... numbers) {
+    private static long[] calculate(int... numbers) {
         if (numbers == null) {
             return null;
         }
@@ -31,41 +31,40 @@ public class FactorialCalculation {
         long[] factorials = new long[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
             long factorial = 1;
-            if (numbers[i] < 0) {
-                continue;
-            }
+            if (numbers[i] < 0) continue;
             for (int j = 1; j <= numbers[i]; j++) {
                 factorial *= j;
             }
 
-            for (int k = 0; k < factorials.length; k++) {
-                factorials[k] = factorial;
-            }
+            factorials[i] = factorial;
         }
         return factorials;
     }
 
-    public static void showCalculation(long[] factorials, int... numbers) {
+    private static void showCalculation(long[] factorials, int... numbers) {
         if (numbers == null) {
-            System.out.println("Никакой информации не получено");
+            System.out.println("Ошибка в данных");
             return;
         }
         if (numbers.length == 0) {
-            System.out.println("Получен массив нулевой длины");
+            System.out.println("Массив нулевой длины");
             return;
         }
 
+        StringBuilder sb = new StringBuilder("! = 1");
         for (int i = 0; i < numbers.length; i++) {
             int n = numbers[i];
-            StringBuilder sb = new StringBuilder("! = 1");
+            sb.delete(5, sb.length());
             if (n > 1) {
-                for (int j = 1; j < n; n++)
+                for (int j = 1; j < n; n++) {
                     sb.append(" * ").append(j + 1);
+                }
+                sb.append(" = ");
             }
             long f = factorials[i];
             System.out.println(n < 0 ? "Ошибка: факториал " + n + "! не определен"
                     : (n == 1) || (n == 0) ? n + "! = 1"
-                    : n + sb.toString() + " = " + f);
+                    : n + sb.toString() + f);
         }
     }
 }
