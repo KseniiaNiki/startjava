dae0611a2ca01089e1e8607dbd683677e4feaec6
Ksenia Nikienko <ksysic@mail.ru>
2025-09-11 00:52:33 +0300

    Редактирование

--- src/ru/topjava/startjava/lesson_2_3_4/array/TypewriterEffect.java
+++ src/ru/topjava/startjava/lesson_2_3_4/array/TypewriterEffect.java
@@ -4,21 +4,25 @@ public class TypewriterEffect {
     public static void main(String[] args) throws InterruptedException {
         String sentence = "Java - это C++, из которого убрали все пистолеты, ножи и дубинки.\n" +
                 "- James Gosling";
-        String[] parts = findShortestAndLongestWords(sentence);
-        printWithEffect(parts);
+        int[] indices = findShortestAndLongestWords(sentence);
+        String[] words = findUpperCaseRange(sentence, indices);
+        type(words);
         sentence = "Чтобы написать чистый код, мы сначала пишем грязный код, затем рефакторим его.\n" +
                 "- Robert Martin";
-        parts = findShortestAndLongestWords(sentence);
-        printWithEffect(parts);
+        indices = findShortestAndLongestWords(sentence);
+        words = findUpperCaseRange(sentence, indices);
+        type(words);
         sentence = null;
-        parts = findShortestAndLongestWords(sentence);
-        printWithEffect(parts);
+        indices = findShortestAndLongestWords(sentence);
+        words = findUpperCaseRange(sentence, indices);
+        type(words);
         sentence = "";
-        parts = findShortestAndLongestWords(sentence);
-        printWithEffect(parts);
+        indices = findShortestAndLongestWords(sentence);
+        words = findUpperCaseRange(sentence, indices);
+        type(words);
     }
 
-    private static String[] findShortestAndLongestWords(String sentence) {
+    private static int[] findShortestAndLongestWords(String sentence) {
         if (sentence == null) {
             System.out.println("Ошибка в получении данных");
             return null;
@@ -33,42 +37,45 @@ public class TypewriterEffect {
 
         String shortestWord = words[0];
         String longestWord = words[0];
-        int shortIndex = 0;
-        int longIndex = 0;
+        int shortestWordIndex = 0;
+        int longestWordIndex = 0;
         for (int i = 0; i < words.length; i++) {
             if (words[i].isBlank()) {
                 continue;
             }
             if (words[i].length() < shortestWord.length()) {
                 shortestWord = words[i];
-                shortIndex = i;
+                shortestWordIndex = i;
             }
             if (words[i].length() > longestWord.length()) {
                 longestWord = words[i];
-                longIndex = i;
+                longestWordIndex = i;
             }
         }
+        return new int[] {shortestWordIndex, longestWordIndex};
+    }
 
+    private static String[] findUpperCaseRange(String sentence, int[] indices) {
+        if ((sentence == null) || (indices == null)) {
+            return null;
+        }
         String[] parts = sentence.split(" ");
-        if (shortIndex < longIndex) {
-            for (int i = shortIndex; i <= longIndex; i++) {
-                parts[i] = parts[i].toUpperCase();
-            }
-        } else {
-            for (int i = longIndex; i <= shortIndex; i++) {
-                parts[i] = parts[i].toUpperCase();
-            }
+        if (indices[0] > indices[1]) {
+            indices[0] = indices[0] ^ indices[1] ^ (indices[1] = indices[0]);
+        }
+        for (int i = indices[0]; i <= indices[1]; i++) {
+            parts[i] = parts[i].toUpperCase();
         }
         return parts;
     }
 
-    private static void printWithEffect(String[] parts) throws InterruptedException {
-        if (parts == null) {
+    private static void type(String[] words) throws InterruptedException {
+        if (words == null) {
             return;
         }
 
-        for (String part : parts) {
-            for (char ch : part.toCharArray()) {
+        for (String word : words) {
+            for (char ch : word.toCharArray()) {
                 System.out.print(ch);
                 Thread.sleep(100);
             }
