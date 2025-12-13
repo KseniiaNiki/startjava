2d755dd7b1fb8728a05c4b17dead8d1886d192c4
Ksenia Nikienko <ksysic@mail.ru>
2025-12-13 17:08:38 +0300

    Доработка

--- src/ru/topjava/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/ru/topjava/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class GuessNumber {
     private final Player playerOne;
     private final Player playerTwo;
+
+    private final int minValue = Player.MIN_VALUE;
+    private final int maxValue = Player.MAX_VALUE;
+    private final int attempts = Player.ATTEMPTS;
+    private final int randomNumber = getRandomNumber(maxValue, minValue);
+
     boolean isGameOver = false;
 
     public GuessNumber(Player playerOne, Player playerTwo) {
@@ -13,58 +19,64 @@ public class GuessNumber {
     }
 
     public void start() {
-        int minValue = Player.MIN_VALUE;
-        int maxValue = Player.MAX_VALUE;
-        int attempts = Player.ATTEMPTS;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Игра началась! У каждого игрока по " + attempts + " попыток");
-        int randomNumber = (int) (Math.random() * (maxValue - minValue + 1)) + minValue;
         System.out.println("Угадайте целое число в отрезке [" + minValue + ", " + maxValue + "]: ");
         while (!isGameOver) {
-            Player[] players = new Player[] {playerOne, playerTwo};
-            if (playerOne.hasAttemptsLeft()) {
+            if (playerOne.hasAttemptsLeft() || playerTwo.hasAttemptsLeft()) {
                 makeMove(playerOne, scanner, randomNumber);
                 if (playerOne.getAttempts()[playerOne.getAttempts().length - 1] == randomNumber) {
-                    endGame(randomNumber, players);
+                    endGame(randomNumber, playerOne, playerTwo);
                     return;
                 }
-            }
-            if (playerTwo.hasAttemptsLeft()) {
                 makeMove(playerTwo, scanner, randomNumber);
-                if (!playerTwo.hasAttemptsLeft() ||
-                        playerTwo.getAttempts()[playerTwo.getAttempts().length - 1] == randomNumber) {
-                    endGame(randomNumber, players);
+                if (playerTwo.getAttempts()[playerTwo.getAttempts().length - 1] == randomNumber) {
+                    endGame(randomNumber, playerOne, playerTwo);
                     return;
                 }
+            } else {
+                endGame(randomNumber, playerOne, playerTwo);
+                return;
             }
         }
     }
 
     private void makeMove(Player player, Scanner scanner, int randomNumber) {
-        System.out.print("Попытка №" + player.getAttemptCount() +
+        System.out.print("Попытка №" + (player.getCurrAttempt() + 1) +
                 "\nЧисло вводит " + player.getName() + ": ");
-        player.setNumber(scanner);
-        if (player.getNumber() == randomNumber) {
-            player.addAttempt(player.getNumber());
-            System.out.println(player.getName() + " угадал число " + player.getNumber() +
+        getValidNumber(scanner, player);
+        if (player.getCurrentNumber() == randomNumber) {
+            System.out.println(player.getName() + " угадал число " + player.getCurrentNumber() +
                     " с " + (player.getCurrAttempt()) + "-й попытки!");
-            return;
         } else {
-            System.out.println("Ваше число " + ((player.getNumber() > randomNumber)
+            System.out.println("Ваше число " + ((player.getCurrentNumber() > randomNumber)
                     ? "больше того, что загадал компьютер"
                     : "меньше того, что загадал компьютер"));
         }
-        player.addAttempt(player.getNumber());
         if (!player.hasAttemptsLeft()) {
             System.out.println("У " + player.getName() + " закончились попытки!");
         }
     }
 
-    private void endGame(int randomNumber, Player[] players) {
+    private void getValidNumber(Scanner scanner, Player player) {
+        int currNumber;
+        boolean isValid = false;
+        while (!isValid) {
+            currNumber = scanner.nextInt();
+            isValid = player.addNumber(currNumber);
+        }
+    }
+
+    private static int getRandomNumber(int maxValue, int minValue) {
+        return (int) (Math.random() * (maxValue - minValue + 1)) + minValue;
+    }
+
+    private void endGame(int randomNumber, Player playerOne, Player playerTwo) {
+        Player[] players = {playerOne, playerTwo};
         System.out.println("Игра окончена! Загаданное число: " + randomNumber);
         printAllAttempts(players);
         for (Player pl : players) {
-            pl.clearAttempts();
+            pl.clear();
         }
         isGameOver = true;
     }
--- src/ru/topjava/startjava/lesson_2_3_4/guess/Player.java
+++ src/ru/topjava/startjava/lesson_2_3_4/guess/Player.java
@@ -1,7 +1,6 @@
 package ru.topjava.startjava.lesson_2_3_4.guess;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Player {
     protected static final int MIN_VALUE = 1;
@@ -9,9 +8,7 @@ public class Player {
     protected static final int ATTEMPTS = 10;
 
     private final String name;
-    private int number;
     private final int[] numbers = new int[ATTEMPTS];
-    private int attemptCount = 1;
     private int currAttempt = 0;
 
     public Player(String name) {
@@ -22,37 +19,36 @@ public class Player {
         return name;
     }
 
-    public int getNumber() {
-        return number;
-    }
-
-    public int getAttemptCount() {
-        return attemptCount;
-    }
-
     public int getCurrAttempt() {
         return currAttempt;
     }
 
-    public void setNumber(Scanner scanner) {
-        int currNumber = 0;
-        boolean isValid = false;
-        while (!isValid) {
-            currNumber = scanner.nextInt();
-            if ((currNumber < MIN_VALUE) || (currNumber > MAX_VALUE)) {
-                System.out.println("Число должно входить в отрезок [" + MIN_VALUE + ", " + MAX_VALUE + "].");
-                System.out.print("Попробуйте еще раз: ");
-            } else {
-                isValid = true;
-            }
+    public boolean addNumber(int number) {
+        if ((number < MIN_VALUE) || (number > MAX_VALUE)) {
+            System.out.println("Число должно входить в отрезок [" + MIN_VALUE + ", " + MAX_VALUE + "].");
+            System.out.print("Попробуйте еще раз: ");
+            return false;
+        } else {
+            addAttempt(number);
+            return true;
         }
-        this.number = currNumber;
+    }
+
+    public int getCurrentNumber() {
+        if (currAttempt <= numbers.length - 1) {
+            return numbers[currAttempt];
+        } else {
+            return numbers[currAttempt - 1];
+        }
+    }
+
+    public boolean hasAttemptsLeft() {
+        return currAttempt < ATTEMPTS;
     }
 
     public void addAttempt(int number) {
         if (currAttempt < ATTEMPTS) {
             numbers[currAttempt++] = number;
-            attemptCount++;
         }
     }
 
@@ -60,13 +56,8 @@ public class Player {
         return Arrays.copyOf(numbers, currAttempt);
     }
 
-    public void clearAttempts() {
-        attemptCount = 1;
+    public void clear() {
         currAttempt = 0;
-        Arrays.fill(numbers, 0);
-    }
-
-    public boolean hasAttemptsLeft() {
-        return currAttempt < ATTEMPTS;
+        Arrays.fill(getAttempts(), 0);
     }
 }
